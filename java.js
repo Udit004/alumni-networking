fccdad994359187e1cef92a1db143d548557ef3c
Satyam8589 <137251288+Satyam8589@users.noreply.github.com>
2024-07-14 17:41:40 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+<script>
+function addToCart(button) {
+    alert("Added to cart!")
+}
+</script>
+
+function openOverlay() {
+    document.getElementById('feedbackOverlay').style.display = 'block';
+}
+
+function closeOverlay() {
+    document.getElementById('feedbackOverlay').style.display = 'none';
+}
